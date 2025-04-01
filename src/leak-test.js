0be78252ea86f76d07a32917a56b7359532d7bf0
Yahye,Mahamed M <m.yahye@student.fontys.nl>
2025-04-01 10:25:24 +0200

    Testing Gitleaks

---
+++ src/leak-test.js
@@ -0,0 +1,5 @@
+// Voorbeeld in JavaScript (leak-test.js)
+const secrets = {
+    awsKey: "AKIAIOSFODNN7EXAMPLE",  // Nep AWS key
+    stripeKey: "sk_test_4eC39HqLyjWDarjtT1zdp7dc"  // Nep Stripe key
+  };
